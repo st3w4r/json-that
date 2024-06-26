24bff533b93d038233181c0df93b4947c078f9bb
Yanael Barbier <y.barbier@sprout.ai>
2024-06-26 17:56:01 +0100

    format

--- jsonthat/bump_version.py
+++ jsonthat/bump_version.py
@@ -1,5 +1,5 @@
 import toml
-import os
+
 
 def read_version_from_pyproject():
     with open("pyproject.toml", "r") as f:
@@ -9,15 +9,18 @@ def read_version_from_pyproject():
         raise ValueError("Version not found in pyproject.toml")
     return version
 
+
 def write_version_to_file(version):
     version_file_content = f'__version__ = "{version}"\n'
     with open("jsonthat/version.py", "w") as f:
         f.write(version_file_content)
 
+
 def main():
     version = read_version_from_pyproject()
     write_version_to_file(version)
     print(f"Version {version} written to version.py")
+
 
 if __name__ == "__main__":
     main()
